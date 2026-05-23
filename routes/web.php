375a539214cf0ce91e0f7bcc5b2ee1b7a52f3542
cjes2475 <sisoncarljusten@gmail.com>
2026-05-23 21:55:38 +0800

    final job list

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\JobController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [JobController::class, 'index'])->name('jobs.index');
+Route::get('/jobs/{job}', [JobController::class, 'show'])->name('jobs.show');
